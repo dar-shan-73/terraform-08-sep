76aac55e1c8297e08d5b1c64f8cda0632eb7b274
dar-shan-73 <naik.darshan73@gmail.com>
2025-09-09 17:01:33 +0530

    added 03

--- 02-ec2/ec2.tf
+++ 02-ec2/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-0b7f8ce024e0f831d"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "my-ec2-instance using TF"

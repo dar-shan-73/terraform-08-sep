23f653e42db5ecf6e7abe4d5467fac508adf42c2
dar-shan-73 <naik.darshan73@gmail.com>
2025-09-10 23:08:44 +0530

    expense app files added

--- 03-expense-v1/ec2.tf
+++ 03-expense-v1/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "frontend" {
   ami = "ami-06f2c6d4587118660"
 
   instance_type          = "t3.micro"
-  vpc_security_group_ids = "sg-0ab72cd13746042fa"
+  vpc_security_group_ids = ["sg-0ab72cd13746042fa"]
 
   tags = {
     Name = "frontend TF-instance"
@@ -13,7 +13,7 @@ resource "aws_instance" "backend" {
   ami = "ami-06f2c6d4587118660"
 
   instance_type          = "t3.micro"
-  vpc_security_group_ids = "sg-0ab72cd13746042fa"
+  vpc_security_group_ids = ["sg-0ab72cd13746042fa"]
 
 
   tags = {
@@ -26,7 +26,7 @@ resource "aws_instance" "mysql" {
   ami = "ami-06f2c6d4587118660"
 
   instance_type          = "t3.micro"
-  vpc_security_group_ids = "sg-0ab72cd13746042fa"
+  vpc_security_group_ids = ["sg-0ab72cd13746042fa"]
 
 
   tags = {

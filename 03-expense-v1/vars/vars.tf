9f8bd794973489e6cc9548692e81764f920ec429
dar-shan-73 <naik.darshan73@gmail.com>
2025-09-15 13:30:27 +0530

    list var details added

--- 03-expense-v1/vars/vars.tf
+++ 03-expense-v1/vars/vars.tf
@@ -7,3 +7,29 @@ output "op" {
     value = var.a  #this is how we access a variable in Terraform
   
 }
+
+
+#data types
+# numbers
+# strings
+# booleans
+
+
+#variables types
+# list - a key with multiple values
+# map - a key with multiple key value pairs
+# dicionary - a key with a value pairs
+
+# declaring a list variable..list always starts with 01234
+variable "sample" {
+    default = [
+        100,
+        "hello",
+        true,
+    ]
+  
+}
+# accesing the list variable
+output "sample_output" {
+    value = var.sample
+}

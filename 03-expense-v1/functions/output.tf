ba665df9e5df7acc159b75fbb396a6c8ccac64cc
dar-shan-73 <naik.darshan73@gmail.com>
2025-09-15 21:14:42 +0530

    functions added

--- 03-expense-v1/functions/output.tf
+++ 03-expense-v1/functions/output.tf
@@ -3,3 +3,8 @@ output "apple" {
   value = "apple color is ${var.fruits["apple"]["color"]} and tastes ${var.fruits["apple"]["taste"]} and price is ${var.fruits["apple"]["price"]}"
 
 }
+
+output "banana" {
+  value = "banana color is ${var.fruits["banana"]["color"]} and tastes ${var.fruits["banana"]["taste"]} and price is ${var.fruits["banana"]["price"]}"
+  
+}
